a3d165f4b689f7d15f10cd9a675f570bb53b01cd
Ahmed Embaby <Ahmed.Embaby@linkdev.com>
2023-07-08 02:47:24 +0300

    first commit

---
+++ ITI_OOP/School/Models/Course.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI_OOP.School.Models
+{
+    internal class Course
+    {
+        public int Id { get;private set; }
+        public string Name { get;private set; }
+        private Student[] Students { set;get; }
+        private const int MAX_ENROLLED_STUDENTS=100;
+        private int NextIndex = 0;
+        public Course(int id,string name) { 
+            this.Name = name;
+            this.Id = id;   
+            Students=new Student[MAX_ENROLLED_STUDENTS];
+        }
+
+        public void EnrollStudent(Student student)
+        {
+            //logic here 
+            if (!CanIEnroll(student))
+            {
+                throw new Exception($"{this.Name} is reached the max allowed students");
+            }
+            this.Students[NextIndex++] = student;
+        }
+
+        public bool CanIEnroll(Student student)
+        {
+            return this.NextIndex != MAX_ENROLLED_STUDENTS;
+        }
+
+    }
+}
---
+++ ITI_OOP/School/Models/Student.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI_OOP.School.Models
+{
+    internal class Student
+    {
+        private string Name { get; set; }
+        private Course[] Courses {set; get; }
+        private int NextCourcesIndex=0;
+        public static int MAX_ALLOWED_COURCES=4;
+        public Student(string name)
+        {
+            Courses = new Course[MAX_ALLOWED_COURCES];
+            Name = name;
+        }
+        public void Enroll(Course course)
+        {
+            if (NextCourcesIndex == MAX_ALLOWED_COURCES)
+            {
+                throw new Exception("reached the max allowed cources");
+            }
+
+            if (IsCourceEnrolled(course,this.Courses))
+            {
+                 throw new Exception($"aleady inrolled in {course.Name}");
+            }
+
+            this.Courses[this.NextCourcesIndex++]=course;
+            course.EnrollStudent(this);
+        }
+
+        private static bool IsCourceEnrolled(Course course, Course[] cources)
+        {
+            foreach (Course c in cources)
+            {
+                if (c != null && course.Id == c.Id)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void PrintCources()
+        {
+            for (int i=0;i<NextCourcesIndex;i++)
+            {
+                Console.WriteLine(this.Courses[i].Name);
+            }
+        }
+    }
+}
